796307c94f8d5616a59f820799c52a910ddeb335
tjex <tjex@tjex.net>
2024-08-07 19:28:57 +0200

    initial test and sudo code skeleton

---
+++ bert/sudo-code-skeleton.py
@@ -0,0 +1,16 @@
+# sudo code skeleton to map out the steps needed as I develop the process.
+
+
+# load models, libs, etc / declare vars / etc
+
+# import / request note data from zk
+
+# prepare note data (extract note body text)
+
+# model.encode(the_zk_text_data) 
+
+# compute similarities
+
+# map vector results to note titles
+
+# return results
