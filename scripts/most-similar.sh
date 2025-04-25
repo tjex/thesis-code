0398c7a98abf3e1da5a47c177e5c5b2e49b68b44
tjex <tjex@tjex.net>
2025-04-25 12:32:51 +0200

    output script

--- scripts/most-similar.sh
+++ scripts/most-similar.sh
@@ -3,4 +3,15 @@
 script_dir=$(dirname "$(realpath "$0")")
 data_dir="$script_dir/../data"
 
-cat "$data_dir/simdiss.json" | jq '.most_similar[].title'
+input_title=$(cat "$data_dir/simdiss.json" | jq '.title' | sed 's/"//g')
+
+most_similar=$(cat "$data_dir/simdiss.json" | jq '.most_similar[].title' | sed 's/"//g')
+least_similar=$(cat "$data_dir/simdiss.json" | jq '.least_similar[].title' | sed 's/"//g')
+
+echo "Input title: $input_title"
+echo ""
+echo "--- Most Similar ---"
+echo "$most_similar"
+echo ""
+echo "--- Least Similar ---"
+echo "$least_similar"
